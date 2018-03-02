303ee9b7a522159cf8ddc908f8fd25a8286f838b
Jeremiah Boby <jerbob17@student.loreto.ac.uk>
2018-03-02 22:35:16 +0000

    Add files via upload

---
+++ content.js
@@ -0,0 +1,4 @@
+var images = document.getElementsByTagName('img');
+for (var i = 0, l = images.length; i < l; i++) {
+  images[i].src = 'https://cdn.discordapp.com/attachments/366673702533988363/419261314540765184/unknown.png';
+}
