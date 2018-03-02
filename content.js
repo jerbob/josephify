279f954ed7bc86c966004f27237d4a28a11d7cec
Jeremiah Boby <jerbob17@student.loreto.ac.uk>
2018-03-02 23:37:58 +0000

    Added a fancy new logo!

--- content.js
+++ content.js
@@ -1,3 +1,6 @@
+chrome.browserAction.setIcon({
+    path : "media/joseph/icon.png"
+});
 var images = document.getElementsByTagName('img');
 for (var i = 0, l = images.length; i < l; i++) {
   images[i].src = 'https://cdn.discordapp.com/attachments/366673702533988363/419261314540765184/unknown.png';
