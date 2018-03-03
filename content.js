0fdaf03c0c7515fbd0e003e6c1c8472be3baf133
Aperture <jeremiahmaniattu@gmail.com>
2018-03-03 02:55:56 +0000

    Fixed absolute paths
    Made the URLs variable to accept multiple ids.

--- content.js
+++ content.js
@@ -1,10 +1,11 @@
 if (document.title === "Google"){
     var inputBoxes = document.getElementsByTagName('input');
     var googleLogo = document.getElementById('hplogo');
+    var googleURL = chrome.extension.getURL('media/joseph/google.png');
     inputBoxes[7].value = 'Dank';
     inputBoxes[8].value = 'Memes';
     googleLogo.remove();
     document.body.style.cssText = 
-    'background: url(chrome-extension://__MSG_@@extension_id__/media/joseph/google.png)'+
+    'background: url('+ googleURL +')'+
     ' no-repeat center center fixed !important';
 }
