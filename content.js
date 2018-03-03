c79f34892dd1106f406b9165ba7c551332c3ca0b
Aperture <jeremiahmaniattu@gmail.com>
2018-03-03 02:50:41 +0000

    Found the problem?
    Used placeholders for the extension ids, rather than the ids themselves.

--- content.js
+++ content.js
@@ -5,6 +5,6 @@ if (document.title === "Google"){
     inputBoxes[8].value = 'Memes';
     googleLogo.remove();
     document.body.style.cssText = 
-    'background: url(chrome-extension://mmollkoehhjoknlfcchioklddkdggaem/media/joseph/google.png)'+
+    'background: url(chrome-extension://__MSG_@@extension_id__/media/joseph/google.png)'+
     ' no-repeat center center fixed !important';
 }
